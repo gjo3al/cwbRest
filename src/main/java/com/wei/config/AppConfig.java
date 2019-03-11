564f7764f7ff5b5f8f8712aea9b2ad1047efbce1
gjo3al <gjo3al@gmail.com>
2019-03-12 02:29:37 +0800

    Refactor -- separate to subprojects

--- src/main/java/com/wei/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.wei.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"com.wei.dao"})
-public class AppConfig {
-}
--- src/main/java/com/wei/dao/ResourceIdDAO.java
+++
@@ -1,9 +0,0 @@
-package com.wei.dao;
-
-import java.util.List;
-
-public interface ResourceIdDAO {
-	public String getResourceId(String area, int period);
-	public List<String> getTowns(String area);
-	public void close();
-}
